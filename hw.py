45e689f577ab882cfa1cb38e3e87ffd9f6a422c4
arman <sadoonarman@icloud.com>
2025-12-04 11:16:19 +0330

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version)
+
+
+#------------------------------------
+#camleCase
+
+num1 = 5
+num2 = 14
+res = num1 + num2
+print(f" result is :  {res}") 
+#------------------------------------
+
+
